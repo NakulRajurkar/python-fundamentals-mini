84ff9c0e2a28959c5ebb0dc0d72f2d7152a2040f
NakulRajurkar <rajurkarnakul091@gmail.com>
2026-08-05 23:13:13 +0530

    Complete farm yield comparison

--- farm_yield.py
+++ farm_yield.py
@@ -1,24 +1,50 @@
-list1=[]
-n=int(input("Enter number of crops:"))
-for i in range(n):
-    crop=input("Enter crop name:")
-    y=float(input("ENter yield:"))
-    data={
-        "crop":crop,
-        "yield":y
-    }
-    list1.append(data)
+def get_records():
+    records = []
+    n=int(input("Enter the number of crops: "))
+    for i in range(n):
+        crop=input("Enter the crop name: ")
+        try:
+            y=float(input("Enter the yield : "))
+            if y>0:
+                data={
+                    "crop": crop,
+                    "yield": y
+                }
+                records.append(data)
+            else:
+                print("Yield must be a positive number. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number for yield.")
+    return records
+print("Enter first set:")
+list1=get_records()
+print("Enter second set:")
+list2=get_records()
 print(list1)
-    
-    
-list2=[]
-n=int(input("Enter number of crops for second record: :"))
-for i in range(n):
-    crop=input("Enter crop name:")
-    y=float(input("ENter yield:"))
-    data={
-        "crop":crop,
-        "yield":y
-    }
-    list2.append(data)
 print(list2)
+def compare_crops(list1, list2):
+
+    for item1 in list1:
+        found = False
+
+        for item2 in list2:
+
+            if item1["crop"] == item2["crop"]:
+
+                difference = item2["yield"] - item1["yield"]
+
+                if difference > 0:
+                    print(item1["crop"], "yield increased by", difference)
+
+                elif difference < 0:
+                    print(item1["crop"], "yield decreased by", abs(difference))
+
+                else:
+                    print(item1["crop"], "yield stayed the same")
+
+                found = True
+                break
+
+        if found == False:
+            print(item1["crop"], "is missing in second set")
+compare_crops(list1, list2)
