8034478d362b88a4752bc48f7dfb53761222304c
NakulRajurkar <rajurkarnakul091@gmail.com>
2026-08-05 22:55:01 +0530

    Add second farm yield record

--- farm_yield.py
+++ farm_yield.py
@@ -11,3 +11,14 @@ for i in range(n):
 print(list1)
     
     
+list2=[]
+n=int(input("Enter number of crops for second record: :"))
+for i in range(n):
+    crop=input("Enter crop name:")
+    y=float(input("ENter yield:"))
+    data={
+        "crop":crop,
+        "yield":y
+    }
+    list2.append(data)
+print(list2)
